a365895ebedc7b7b6cccffca43d849fb05fe4f55
Wille Lee <1992season@gmail.com>
2022-06-18 01:24:38 +0900

    added

---
+++ level-1/행렬덧셈.js
@@ -0,0 +1,31 @@
+/*
+행렬의 덧셈은 행과 열의 크기가 같은 두 행렬의 같은 행, 같은 열의 값을 서로 더한 결과가 됩니다. 2개의 행렬 arr1과 arr2를 입력받아, 행렬 덧셈의 결과를 반환하는 함수, solution을 완성해주세요.
+*/
+// my sol
+function solution(arr1, arr2) {
+  const result = [];
+  for (let i = 0; i < arr1.length; i++) {
+    const currRow = [];
+    for (let j = 0; j < arr1[i].length; j++) {
+      currRow.push(arr1[i][j] + arr2[i][j]);
+    }
+    result.push(currRow);
+  }
+  return result;
+}
+
+//simpler
+function solution2(arr1, arr2) {
+  const result = [];
+  for (let i = 0; i < arr1.length; i++) {
+    result[i] = [];
+    for (let j = 0; j < arr1[i].length; j++) {
+      result[i].push(arr1[i][j] + arr2[i][j]);
+    }
+  }
+  return result;
+}
+
+function solution3(arr1, arr2) {
+  return arr1.map((a, i) => a.map((b, j) => b + arr2[i][j]));
+}
